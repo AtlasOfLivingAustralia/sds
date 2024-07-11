7f86c61752a7337ee922be53f1c164e4f0d210a5
Nick dos Remedios <nick.dosremedios@csiro.au>
2024-07-11 13:50:26 +1000

    Remove version number hard-coded in sh script

--- createxml.sh
+++ createxml.sh
@@ -1,4 +1,25 @@
+#!/bin/bash
+
+# Script to generate the sensitivie-species.xml file.
+#
+# Requires a config file to be present in /data/sds/sds-config.properties
+# Code must be built with `mvn clean install` 
+# Will output the raw XML string to stdout
+
 cd target
-jar xf sds-1.0-SNAPSHOT-assembly.jar lib lib
-java -classpath sds-1.0-SNAPSHOT.jar:lib au.org.ala.sds.util.SensitiveSpeciesXmlBuilder
+
+# These two lines are used to find the LATEST version jar files in target dir (with and without assembly)
+jar_file=$(find . -maxdepth 1 -type f -regex '\./sds.*\.jar' | sort | grep -v 'assembly' | head -n 1) 
+assembly_file=$(find . -maxdepth 1 -type f -regex '\./sds.*\.jar' | sort | grep 'assembly' | head -n 1)
+
+if [ -z "$assembly_file" ] && [ -z "$jar_file" ]; then
+  echo "No file matching the pattern was found."
+else
+  # echo "File found: $jar_file"
+  # echo "File found: $assembly_file"
+  # Create the lib directory and run the SensitiveSpeciesXmlBuilder "main" class
+  jar xf $assembly_file lib lib 
+  java -Xmx2g -Xms2g -classpath "${jar_file}:lib/*" au.org.ala.sds.util.SensitiveSpeciesXmlBuilder # > sensitive-species-data.xml
+fi
+
 cd ..
